522a1171eda1581713006ac28d66da01197a346a
Melissa <mel.layn2006@gmail.com>
2024-07-12 18:36:29 -0300

    adicionando os paragrafos

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,12 +5,24 @@ function Home() {
 
             <div className="justificar">
                 <div className="escuro">
-
+                    
                     <div className="texto">
 
+                    <div className="ajuste">
+
+                    <div className="titulo">
                         <h1>
                             Buddy
-                        </h1></div>
+                        </h1>
+                    </div>
+                        <div className="primeiro_paragrafo">
+
+                            <p>Robô que auxilia responsáveis no desenvolvimento da fala, interação social e reconhecimento de emoções de crianças autistas de nível 2 entre 5 e 10 anos de idade.  Possui integração com App, o que facilita a devolução de relatórios que analisam a evolução do autista.</p>
+                        </div>
+
+                    </div>
+</div>
+
 
 
 
